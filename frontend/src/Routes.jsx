441f2e6a0cb97cf90ea66b24379797d041d6eb78
penumajji sravani <penumajjisravani698@gmail.com>
2025-02-11 15:02:19 +0530

    added signup page

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -1,3 +1,18 @@
-import LoginPage from './pages/Login'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoginPage from './Pages/Login';
+import SignUpPage from './Pages/Signup.jsx'
 
-export {LoginPage};
+
+const AppRoutes = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRoutes;
