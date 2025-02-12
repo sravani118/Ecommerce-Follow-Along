00273e6a9e81eebed0ea6e142ef87da281725748
penumajji sravani <penumajjisravani698@gmail.com>
2025-02-12 13:46:40 +0530

    added backend for signup

--- backend/routes/routes.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/controller');
-
-router.get('/data', controller.getData);
-module.exports = router;
