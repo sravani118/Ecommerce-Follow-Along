140b61a4603ce48b178b02246b8938b31ba84177
penumajji sravani <penumajjisravani698@gmail.com>
2025-03-03 10:44:28 +0530

    changes of milestone 12

---
+++ frontend/src/components/auth/Showcase.jsx
@@ -0,0 +1,9 @@
+function Showcase() {
+    return (
+      <div className='bg-gray-700'>
+        <img src="https://cdn.shopify.com/s/files/1/0646/3222/5972/files/download-20.jpg?v=1714388022" alt="showcase 1" className='w-[90%] flex justify-center m-auto' />
+      </div>
+    )
+  }
+  
+  export default Showcase
