098f6be89039119e451eee938b8fae590aad5f1f
penumajji sravani <penumajjisravani698@gmail.com>
2025-03-14 14:45:02 +0530

    added endpoint for cart

--- frontend/src/components/auth/Showcase.jsx
+++ frontend/src/components/auth/Showcase.jsx
@@ -1,9 +1,11 @@
+// import localbazarImage from "../../assets/localbazar.png";
+
 function Showcase() {
-    return (
-      <div className='bg-gray-700'>
-        <img src="https://cdn.shopify.com/s/files/1/0646/3222/5972/files/download-20.jpg?v=1714388022" alt="showcase 1" className='w-[90%] flex justify-center m-auto' />
-      </div>
-    )
-  }
-  
-  export default Showcase
+  return (
+    <div className='bg-[#13234f]'>
+      {/* <img src={} alt="showcase 1" className='w-[90%] flex justify-center m-auto' /> */}
+    </div>
+  );
+}
+
+export default Showcase;
